398ad2292a47afb2031d04df6e94d4601958b91b
Lorenzo Ricolfi <z5391762@ad.unsw.edu.au>
2022-03-15 13:28:21 +1100

    some new variables

--- R/Taxa.R
+++ R/Taxa.R
@@ -14,7 +14,6 @@ new_taxa_data %>%
   summarise(percent = 100 * n() / nrow(new_taxa_data))
 
 #n lines 288:290 and 357 there is no value. I'll just add a random value. 
-
 new_taxa_data[288,3] <- "Other"
 new_taxa_data[289,3] <- "Bird"
 new_taxa_data[290,3] <- "Fish"
@@ -24,11 +23,13 @@ percent <- new_taxa_data %>%
   group_by(Taxa) %>% 
   summarise(percent = 100 * n() / nrow(new_taxa_data))
 
+#pie chart
 ggplot(percent, aes(x = "", y = percent, fill = Taxa)) +
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y", start = 0) +
   theme(axis.title = element_blank())
 
+#histogram
 ggplot(new_taxa_data) + 
   geom_bar(aes(Year, fill = Taxa)) +
   labs(title = "Taxa Dataframe",
@@ -44,6 +45,26 @@ ggplot(new_taxa_data) +
         legend.text = element_text(size = 11), 
         panel.grid.major = element_line(size = 0.1, color = "black"), 
         panel.grid.minor = element_line(size = 0.05, color = "gray"))
+
+#create new variables (PFAS compounds) filled with random numbers (random concentrations)
+PFOA <- seq(from = 0, to = 10, by = .01) 
+PFOS <- seq(from = 0, to = 15, by = .01)
+FOSA <- seq(from = 0, to = 5, by = .01)
+new_taxa_data$PFOA <- sample(PFOA, size = nrow(new_taxa_data), replace = T)
+new_taxa_data$PFOS <- sample(PFOS, size = nrow(new_taxa_data), replace = T)
+new_taxa_data$FOSA <- sample(FOSA, size = nrow(new_taxa_data), replace = T)
+
+#create a new variable (first author's country) filled with randomly chosen categorical data
+new_taxa_data$Country_first_author <- sample(c("Italy", "UK", "USA", "Germany", "Norway", "Greece", "Japan", "Australia", "Alaska", "Faroe_islands", "Poland", "China", "Russia"), size = nrow(new_taxa_data), replace = T)
+
+ggplot(new_taxa_data) +
+  geom_point(aes(x = Year, y = count, color = Taxa))
+
+
+
+
+
+
 
 
 
